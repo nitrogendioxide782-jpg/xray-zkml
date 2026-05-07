8c7d5f9e078918ea9c283d8c0a6db092427e8ae3
nitrogendioxide782-jpg <nitrogendioxide782@gmail.com>
2026-05-08 01:53:28 +0800

    Update ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import threading
-
+st.write("UI OK")
+st.stop()
 from zone1_ai_fhir import load_image
 from zone2_ai_fhir import XRayModel
 from zone3_ai_fhir import generate_report
